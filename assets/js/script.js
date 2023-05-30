50bee17743b2125e65fb2ffc243fdfe1dcfb6dc8
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-05-29 22:20:27 -0400

    write function to fetch data from the geoCode and forecast API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,68 @@
-var searchHitory = $('.search-history');
+var searchHistory = $('.search-history');
 var submitButton = $('form');
 var cityInput = $('#citySearch');
+var myApiKey = 'f49ee689d4dabda3b9e19b6dac6d6729';
 
 // Saves searched cities in the localstorage, clears the input field and calls the renderSearchHistory function
 function saveCities(event) { 
     event.preventDefault();
     var cityValue = cityInput.val().trim();
 
+
     if(cityValue.trim() === "") {
         console.log("Please provide a valid cityname!");
     } else {
+        var geoCodeUrl ='https://api.openweathermap.org/geo/1.0/direct';
+        var geoCodeApi ='${geoCodeUrl}?q=${cityValue}&limit=1&appid=${myApiKey}';
+
+        fetch(geoCodeApi)
+            .then(function (response) {
+            return response.json();
+            })
+            .then(function (data){
+            // get latitude and longtitude from the geocodingAPI response
+            var latitude = data[0].lat;
+            var longtitude = data[0].lon;
+
+            var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast';
+            var forecastApi = '${forecastUrl}?lat=${latitude}&lon={longtitude}&appid=${myApiKey}';
+
+            fetch(forecastApi)
+                .then(function (response) {
+                return response.json(); 
+                })
+                .then(function (data) {
+                    var forecastList = data.list;
+                    forecastList.forEach(function (forecast) {
+                        var name = data.city.name; 
+                        var date = forecast.dt_txt;
+                        var temperature = forecast.main.temp;
+                        var windspeed = forecast.wind.speed;
+                        var humidity = forecast.main.humidity;
+                        var weatherCondition = forecast.weather[0].desctiption;
+
+                        console.log("City Name:", name);
+                        console.log("Date:", date);
+                        console.log("Temperature:", temperature);
+                        console.log("Windspeed:", windspeed);
+                        console.log("Humidity:", humidity);
+                        console.log("Weather Condition:", weatherCondition);
+                    });
+                })
+
+                .catch(function(error){
+                    console.log('Error:', error);
+                });
+
+
+
+
+          })
+          .catch(function(error) {
+            console.log('Error:', error);
+          });
+
+
         localStorage.setItem("city", cityValue);
         renderSearchHistory();
         cityInput.val('');
@@ -27,7 +80,7 @@ function renderSearchHistory() {
     savedCityButton.addClass('btn btn-secondary');
 
     savedCity.append(savedCityButton);
-    searchHitory.append(savedCity);
+    searchHistory.append(savedCity);
 };
 
 
